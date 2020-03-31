cc50a60702de598f59984cf8bf110956746357c2
rkxie <rkxie@amarsoft.com>
2020-03-31 11:41:22 +0800

    中缀表达式转后缀表达式

--- rc_charts/index.js
+++ rc_charts/index.js
@@ -26,22 +26,86 @@ var rootData = {
         }]
     }]
 };
-const opts = ['+','-','/','*'];
-const expressItem = '( ( express1 * ( express2_1 + express2_2 ) ) + ( express3 * express4 ) )';
+var opts = ['+','-','/','*'];
+var expressItem = '( ( _express1_ * ( _express21_ + _express22_ ) ) + ( _express3_ * _express4_ ) )';
+var expressItem_origin = [ '(','(','_express1_','*','(','_express21_','+','_express22_',')',')','+','(','_express3_','*','_express4_',')',')' ];
+
+// console.log('split expression: ', expressItem.replace(/\s*/g,"").split(/[+,-,*,/,(,)]/))
 
 function getTreeD(expression){
-    const temp1 = [], temp2 = [];
+    var temp1 = [], temp2 = [];
+    expression.forEach(ele => {
+        if(ele.match(/^_(.*)_$/)){
+            temp2.push(ele);
+        } else if(isOperator(ele)) {
+            if('(' === ele){
+                temp1.push(ele);
+            } else if(')' === ele){
+                while(temp1.length){
+                    let op = temp1[temp1.length - 1];
+                    if(op === '('){
+                        temp1.pop();
+                        break;
+                    } else {
+                        temp2.push(temp1.pop());
+                    }
+                }
+            } else {
+                while(temp1.length){
+                    if(temp1[temp1.length - 1] === '('){
+                        temp1.push(ele);
+                        break;
+                    } else if(isGreat(temp1[temp1.length - 1], ele)){
+                        temp2.push(temp1.pop());
+                    } else if(isGreat(ele, temp1[temp1.length - 1])){
+                        temp1.push(ele);
+                        break;
+                    }
+                }
+                if(!temp1.length){
+                    temp1.push(ele);
+                }
+            }
+        }   
+    });
+    //将剩余的操作符入栈
+    while(temp1.length){
+        temp2.push(temp1.pop());
+    }
+    //将逆波兰式转化为二叉树
     
 }
 
+
+var resultSuffix = getTreeD(expressItem_origin);
+
+console.log('resultSuffix: ', resultSuffix);
+
+
+function isGreat(op1, op2){
+    if(getPriority(op1) > getPriority(op2)){
+        return true;
+    }
+    return false;
+}
+
+function getPriority(opt){
+    if(opt === '+' || opt === '-'){
+        return 1;
+    }else if(opt === '*' || opt === '/'){
+        return 2;
+    }
+    return new Error('Unsupported operator!');
+}
+
 function isOperator(opt){
-    if(opt === '+' || opt === '-' || opt === '*' || opt === '/'){
+    if(opt === '+' || opt === '-' || opt === '*' || opt === '/' || opt === '(' || opt === ')'){
        return true; 
     }
     return false;
 }
 
-const expressArr = [];
+var expressArr = [];
 
 function getExpression(root){
     if(root !== null){
@@ -58,12 +122,15 @@ function getExpression(root){
 }
 
 // getExpression(rootData);
+// console.log('origin_result: ', expressArr);
 // console.log('result: ', expressArr.join(' '));
+
+
 
 
 function getExpress(treeD){
     let item = treeD;
-    const ret = [], temp = [];
+    var ret = [], temp = [];
     while(item || temp.length > 0){
         item && temp.push(item);
         while(item && item.children){
@@ -82,17 +149,17 @@ function getExpress(treeD){
 
 
 
-// const w = 500;
-// const h = 50;
+// var w = 500;
+// var h = 50;
 
-// const dataset = [5,10,15,20,25];
+// var dataset = [5,10,15,20,25];
 
-// const svg = d3.select("body")
+// var svg = d3.select("body")
 //     .append("svg")
 //     .attr("width", 500)
 //     .attr("height", 50);
 
-// const circles = svg.selectAll('circle')
+// var circles = svg.selectAll('circle')
 //     .data(dataset)
 //     .enter()
 //     .append('circle');
