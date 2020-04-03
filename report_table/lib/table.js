38129284919f9be978559ac71e526d7539301251
rkxie <rkxie@amarsoft.com>
2020-04-03 11:30:33 +0800

    实现table的更新

--- report_table/lib/table.js
+++ report_table/lib/table.js
@@ -1,18 +1,19 @@
 ;(function(){
     'use strict'
+    let _this = null;
     function _getType(value){
         return Object.prototype.toString.call(value).slice(8, -1)
     }
     function _throwError(info){
         throw new Error(info)
     }
-    function _createCaption(data){
+    function _createCaption(tbBox,data){
         const _caption = document.createElement('caption')
         _caption.innerHTML = data['name']
         _caption.setAttribute('class','rc-table-caption')
-        return _caption
+        tbBox.appendChild(_caption)
     }
-    function _createColgroup(headers){
+    function _createColgroup(tbBox,headers){
         if(_getType(headers) !== 'Array'){
             _throwError('table headers is not an array!')
         }
@@ -26,9 +27,9 @@
             }
             _colgroup.appendChild(_col)
         }
-        return _colgroup
+        tbBox.appendChild(_colgroup)
     }
-    function _createHeader(headers){
+    function _createHeader(tbBox,headers){
         if(_getType(headers) !== 'Array'){
             _throwError('table headers is not an array!')
         }
@@ -42,9 +43,9 @@
         }
         _thead.appendChild(_tr)
         _thead.setAttribute('class','rc-table-head')
-        return _thead
+        tbBox.appendChild(_thead)
     }
-    function _createBody(body,headers){
+    function _createBody(tbBox,body,headers){
         if(_getType(body) !== 'Array'){
             _throwError('datasource is not an array!')
         }
@@ -61,33 +62,50 @@
             _tbody.appendChild(_tr);
         }
         _tbody.setAttribute('class','rc-table-body');
-        return _tbody;
+        tbBox.appendChild(_tbody);
     }
     function _createPlusOpt(tbBox){
         const _rt = document.createElement('span');
         const _lb = document.createElement('span');
         _rt.setAttribute('class','rc-table-plus rc-table-rt')
         _lb.setAttribute('class','rc-table-plus rc-table-lb')
+        _rt.setAttribute('key','rc-rt')
+        _lb.setAttribute('key','rc-lb')
+        _rt.onclick = _plusClick
+        _lb.onclick = _plusClick
         tbBox.appendChild(_rt)
         tbBox.appendChild(_lb)
     }
-    function _createTable(data,root){
-        const rootCta = document.getElementById(root)
-        const tbCta = document.createElement('div')
-        const tbBox = document.createElement('table')
-        tbBox.appendChild(_createCaption(data))
-        tbBox.appendChild(_createColgroup(data['headers']))
-        tbBox.appendChild(_createHeader(data['headers']))
-        tbBox.appendChild(_createBody(data['subjects'], data['headers']))
-        _createPlusOpt(tbBox)
-        tbCta.appendChild(tbBox)
-        rootCta.appendChild(tbCta)
-        tbCta.setAttribute('class','rc-tbcta');
-        tbBox.setAttribute('class','rc-table');
+    function _updateTable(data){
+        if(_this.tbBox){
+            _this.tbBox.parentNode.removeChild(_this.tbBox)
+        }
+        _this.tbBox = document.createElement('table')
+        _createCaption(_this.tbBox,data)
+        _createColgroup(_this.tbBox,data['headers'])
+        _createHeader(_this.tbBox,data['headers'])
+        _createBody(_this.tbBox,data['subjects'], data['headers'])
+        _createPlusOpt(_this.tbBox)
+        _this.tbCta.appendChild(_this.tbBox)
+        _this.rootCta.appendChild(_this.tbCta)
+        _this.tbCta.setAttribute('class','rc-tbcta');
+        _this.tbBox.setAttribute('class','rc-table');
+        _this.tbBox.setAttribute('id','rc-my');
+    }
+    function _plusClick(e){
+        console.log('yuyasuiiuasui',e.target.attributes.key.value)
+        _updateTable(_this.dataSource)
+    }
+    function _createTable(_this, data,root){
+        _this.rootCta = document.getElementById(root)
+        _this.tbCta = document.createElement('div')
+        _updateTable(data)
     }
     const _reportTable = {
         createTable: function(data, root){
-            _createTable(data, root)
+            _this = this
+            _this.dataSource = data
+            _createTable(_this, data, root)
         }
     };
     if(typeof module !== 'undefined' && module.exports){
