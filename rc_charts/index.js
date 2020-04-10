7bc8127f6aaeb189fa00f0867f022482d2b3ae42
rkxie <rkxie@amarsoft.com>
2020-04-10 10:37:45 +0800

    新增binaryTree file

--- rc_charts/index.js
+++ rc_charts/index.js
@@ -157,9 +157,5 @@ const mainsel = [
     {fnastatDefName: "利润表", fnastatDefId: "FS000Profit", bool: false}
 ];
 const assistsel = [];
-const expressions = '(Math.abs(#{a.b.c} - #{d.e.f}) * #{g.u.p})'
 
-const exptarr = ['(','Math.abs(','#{a.b.c}', '-', '#{d.e.f}',')', '*', '#{g.u.p}',')']
-
-const finalarr = ['(','(','#{a.b.c}', '-绝对值', '#{d.e.f}',')', '*', '#{g.u.p}',')']
 
--- rc_charts/pre_index.js
+++ rc_charts/pre_index.js
@@ -186,3 +186,6 @@ function getExpress(treeD){
 //         return d;
 //     })
 
+const expressions = '(Math.abs(#{a.b.c} - #{d.e.f}) * #{g.u.p})'
+const exptarr = ['(','Math.abs(','#{a.b.c}', '-', '#{d.e.f}',')', '*', '#{g.u.p}',')']
+const finalarr = ['(','(','#{a.b.c}', '-绝对值', '#{d.e.f}',')', '*', '#{g.u.p}',')']
